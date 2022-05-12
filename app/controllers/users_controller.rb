0954b2afe87cf05f37ec87ec32d3da41cf238cd9
Ali Sajid <alisajidsiddique@gmail.com>
2022-05-12 15:11:05 +0500

    Added error flash functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,20 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:notice]= "You have successfully Signed Up. Welcome to the Alpha Blog #{@user.username}"
+            redirect_to articles_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username,:email,:password)
+    end
+
 end
