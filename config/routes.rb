6fd348bd84a2ac9acb46ba5f0a4e844bc9c8e68b
Ali Sajid <alisajidsiddique@gmail.com>
2022-05-17 16:25:54 +0500

    Added Login and sessions functionality

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
   #post 'users', to: 'users#create'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'     # Cannot use resources for this route as it does not involve database
+  delete 'logout', to: 'sessions#destroy'
 end
