6343cc5d552d854972c5dae8566040d237213479
Ali Sajid <alisajidsiddique@gmail.com>
2022-05-18 15:38:00 +0500

    Added delete user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
-    before_action :set_user, only: [:show, :edit, :update]
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
     before_action :require_user, only: [:edit, :update]
-    before_action :require_same_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def new
         @user = User.new
@@ -39,6 +39,13 @@ class UsersController < ApplicationController
         else
             render :new, status: :unprocessable_entity
         end
+    end
+
+    def destroy
+        @user.destroy
+        session[:user_id] = nil
+        flash[:notice] = "Account and all associate articles have been deleted"
+        redirect_to root_path
     end
 
     private
