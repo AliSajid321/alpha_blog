7bf593d493f82ceb5bc5a31f9fb3f60af19cd59a
Ali Sajid <alisajidsiddique@gmail.com>
2022-05-19 11:32:36 +0500

    Added categories table

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,3 @@
+class CategoriesController < ActionController
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'     # Cannot use resources for this route as it does not involve database
   delete 'logout', to: 'sessions#destroy'
+  resources :categories, except: [:destroy]
 end
